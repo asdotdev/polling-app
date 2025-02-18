a12e74611f8ffe333f72830d5deb1b80019276e6
amitabhsahu <amitabh.sahu0008@gmail.com>
2025-02-18 23:38:12 +0530

    update

--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import connectToDatabase from "@/lib/mongodb";
-import Poll from "@/models/Poll";
-
-export async function GET() {
-    await connectToDatabase();
-    const polls = await Poll.find();
-    return new Response("Vercel", Response.json(polls));
-}
